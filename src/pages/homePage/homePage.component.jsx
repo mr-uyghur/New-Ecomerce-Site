f096c388f1fd8c1bbab67caba142252f7ec8388f
alimran halmamat <mr.uygur1996@gmail.com>
2021-12-01 22:37:01 -0800

    updated over file structure, added dynamic data render with to the front-end with state

--- src/pages/homePage/homePage.component.jsx
+++ src/pages/homePage/homePage.component.jsx
@@ -1,58 +1,10 @@
 import React from 'react';
 import './homePage.styles.scss'
+import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => (
     <div className="homepage">
-        <div className="directory-menu">
-
-    
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                        Hats
-                    </h1>
-                    <span className="subtitle">Shop now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                        Jacket
-                    </h1>
-                    <span className="subtitle">Shop now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                        Sneakers
-                    </h1>
-                    <span className="subtitle">Shop now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                      Womens
-                    </h1>
-                    <span className="subtitle">Shop now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                    Mens
-                    </h1>
-                    <span className="subtitle">Shop now</span>
-                </div>
-            </div>
-
-        </div>
+      <Directory/>
     </div>
 )
 
