80860408f9037ee7a4a1d83e17cf490c1253d055
alimran halmamat <mr.uygur1996@gmail.com>
2021-12-11 01:22:47 -0800

    started adding dynamic data and logic for product preview page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import SHOP_DATA from './shop.data.js'
+import CollectionPreview from '../../components/preview-collection/collection-preview.component'
 
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            collection:SHOP_DATA
+            collections:SHOP_DATA
         }
     }
 
     render() {
+        const {collections }= this.state
+
         return(
-            <div>
-                <h1> ShopPage </h1>
+            <div className = "shop-page">
+                { 
+                    collections.map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview key = {id} {...otherCollectionProps}/>
+                    ))
+                }
                 </div>
         )
     }
