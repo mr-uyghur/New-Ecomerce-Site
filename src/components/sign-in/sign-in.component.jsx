cc762e3f4ca27b8286384f81a973771ae087043e
alimran halmamat <mr.uygur1996@gmail.com>
2021-12-15 20:43:43 -0800

    added reuseble custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 import "./sign-in.styles.scss";
 
 class SignInComponent extends React.Component {
@@ -51,7 +52,7 @@ class SignInComponent extends React.Component {
           />
          
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton  type="submit" >Sign in</CustomButton>
         </form>
       </div>
     );
