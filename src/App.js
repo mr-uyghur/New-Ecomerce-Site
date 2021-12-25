6bf2bb5469b742c781e91ffca1487fe1e5f5b5c4
alimran halmamat <mr.uygur1996@gmail.com>
2021-12-25 15:02:47 -0800

    converted App.js to a class component, added auth library from firebase to authenticate user and user state changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,39 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component'
 import "./App.css";
 import {  Routes, Route} from "react-router-dom";
+import {auth } from './firebase/firebase.utils'
+
+//store the user login info in App state so I can pass them down to other components
 
 
 
+class App extends React.Component {
+//this block of code make the app be aware of any authentication changes in the firebase.
+//OAuth login. User can login with 3rd part accounts
+// -------------------------------------------------------------------------------------------
+  constructor(props) {
+    super()
 
-function App() {
+    this.state = {
+      currentUser:null
+    }
+  }
+
+  unSubscribeFromAuth = null
+
+  //this is auth method we get from Firebase, used for authenticating Signed in users
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser:user})
+    })
+  }
+
+  componentWillUnmount(){
+    this.unSubscribeFromAuth()
+  }
+//--------------------------------------------------------------------------------------------------
+
+render(){
   return (
     <div>
     <Header />
@@ -24,6 +52,7 @@ function App() {
     </Routes>
   </div>
   )
+}
 }
 
 export default App;
