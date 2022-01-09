8649a9cdc34061e6b9e884fde82702b4f3534396
alimran halmamat <mr.uygur1996@gmail.com>
2022-01-09 01:21:11 -0800

    updated fire logic for user Auth

--- src/App.js
+++ src/App.js
@@ -8,31 +8,37 @@ import {  Routes, Route} from "react-router-dom";
 import {auth,createUserProfileDocument } from './firebase/firebase.utils'
 
 //store the user login info in App state so I can pass them down to other components
-
-
-
 class App extends React.Component {
 //this block of code make the app be aware of any authentication state changes in the firebase.
 //OAuth login. User can login with 3rd part accounts
 // -------------------------------------------------------------------------------------------
   constructor(props) {
     super()
-
     this.state = {
       currentUser:null
     }
   }
-
   unSubscribeFromAuth = null
-
   //this is auth method we get from Firebase, used for authenticating Signed in users
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)
-
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
+        /* The code recieves user data from the snapShot object in firebase library */
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      else{
+        this.setState({currentUser:userAuth})
+      }
     })
   }
-
   componentWillUnmount(){
     this.unSubscribeFromAuth()
   }
@@ -44,12 +50,8 @@ render(){
     <Header currentUser = {this.state.currentUser} />
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-    
       <Route path="/shop" element={<ShopPage/>} />
-
-      
       <Route path="/signin" element={<SignInAndSignUpPage/>} />
-      
     </Routes>
   </div>
   )
