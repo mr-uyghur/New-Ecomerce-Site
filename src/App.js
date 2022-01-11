a01ed8041ac2aa7f29ff4b859221d8abc98c0fb4
alimran halmamat <mr.uygur1996@gmail.com>
2022-01-10 23:24:02 -0800

    added sign-up components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./pages/homePage/homePage.component";
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+
 import Header from './components/header/header.component'
 import "./App.css";
 import {  Routes, Route} from "react-router-dom";
