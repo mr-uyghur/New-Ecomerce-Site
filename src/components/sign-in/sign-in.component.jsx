b4f5044fa75ccab189aa70015a3b93e8b8cd4c26
alimran halmamat <mr.uygur1996@gmail.com>
2022-01-15 00:24:21 -0800

    finished sign in handlSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in.styles.scss";
 //import firebase/google user authentication
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignInComponent extends React.Component {
   constructor(props) {
@@ -14,10 +14,16 @@ class SignInComponent extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-
-    this.setState({ email: "", password: "" });
+    const {email, password} = this.state
+    try {
+      await auth.signInWithEmail(email, password)
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
+    
   };
 
   //handle change pulls both value and name from the event target and set their value to the state dynamicaly
