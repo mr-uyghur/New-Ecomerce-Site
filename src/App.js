9ec945068577f736cccdbf946b49641c26fa1ebd
alimran halmamat <mr.uygur1996@gmail.com>
2022-03-05 00:54:45 -0800

    added checkout component and code to render items on the checkout page UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import HomePage from "./pages/homePage/homePage.component";
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {connect} from 'react-redux';
+import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component'
+
+import {connect} from 'react-redux';
 import "./App.css";
 import {  Routes, Route,Navigate } from "react-router-dom";
 import {auth,createUserProfileDocument } from './firebase/firebase.utils'
@@ -49,6 +51,7 @@ render(){
     <Routes>
       <Route exact path="/" element={<HomePage />} />
       <Route path="/shop" element={<ShopPage/>} />
+      <Route exact path="/checkout" element={<CheckoutPage/>} />
       <Route
     path="signIn"
     element={
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import {createStructuredSelector} from 'reselect'
-import CustomButton from '../custom-button/custom-button.component';
-import CartItem from '../cart-item/cart-item.component';
-import {selectCartItems} from '../../redux/cart/cart.selectors'
-import './cart-dropdown.styles.scss';
+import { createStructuredSelector } from "reselect";
+import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { useNavigate } from 'react-router-dom'
+import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
-      {/* the code below, cartItems props is destructured data from redux
-            it'll render the items in the dropdown for shopping cart
-         */}
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropdown = ({ cartItems }) => {
+  let navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/checkout");
+  };
+  return(
+  
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {/* the code below, cartItems props is destructured data from redux
+              it'll render the items in the dropdown for shopping cart
+           */}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message"> Your cart is empty!</span>
+        )}
+      </div>
+      <CustomButton onClick={handleClick}>GO TO CHECKOUT</CustomButton>
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
- 
+  );
+}
+
 //this code gets the cart items data from the redux
 const mapStateToProps = createStructuredSelector({
-    cartItems:selectCartItems
-  });
+  cartItems: selectCartItems,
+});
 
 export default connect(mapStateToProps)(CartDropdown);
